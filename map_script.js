2a8fded1471a42859b33416dce7d2f2eac9382b4
ktr-skmt <sakamoto_911@hotmail.com>
2012-11-06 11:33:27 +0900

    coloring

--- map_script.js
+++ map_script.js
@@ -3,33 +3,101 @@ var ids = {A:0,B:0,E:1,CD:2,K:3,L:3,M:3,F:4,"P-1":4,"P-2":4,Q:4,U:4,W:4,G:5,N:6,
     seats = new Array(),
     is_msie = $.browser.msie && window.XDomainRequest,
     url = base_url + 'room=all',
-    locale = 'ja';
+    en = 'en',
+    roomE = 'E',
+    locale = 'en',
+    colors = new Array(),
+    rooms = get_rooms();
 if (is_msie) {
   var xdr = new XDomainRequest();
   xdr.onload = function() {
     seats = jQuery.parseJSON(xdr.responseText);
+    colors = get_colors();
+    set_colors();
   }
   xdr.open("get", url, true);
   xdr.send(null);
 } else {
-  jQuery.getJSON(url, function (data) {seats = data;});
+  jQuery.getJSON(url, function (data) {
+    seats = data;
+    colors = get_colors();
+    set_colors();
+  });
 }
-function in_session_mode_content(subject, lecturer, suffix) {
-  return subject + "(" + lecturer + ")" + suffix;
+function set_colors() {
+  for (var i = 0, room; room = rooms[i]; i++) {
+    var nodeb = get_nodeb(room), color = colors[i];
+    nodeb.setAttribute("fill", color);
+  }
 }
-function open_mode_content(prefix, num_of_available_seats, num_of_total_seats, available_still) {
-  return prefix + ":<tspan font-weight=\"bold\" fill=\"red\">" + num_of_available_seats + "</tspan>/" + num_of_total_seats + " now-" + available_still;
+function convert_base10to16(base10) {
+  var base16 = base10.toString(16);
+  if (base16.length == 0) {
+    base16 = "00";
+  } else if (base16.length == 1) {
+    base16 = "0" + base16;
+  } else if (base16.length > 2) {
+    if (base16.charAt(1) == '.') {
+      base16 = "0" + base16.charAt(0);
+    } else {
+      base16 = base16.slice(0, 2);
+    }
+  }
+  return base16;
 }
-function closed_mode_content(pronoun, opening_hours_are, opening_time, closing_time, period) {
-  return pronoun + opening_hours_are + opening_time + "-" + closing_time + period;
+function get_color(num_of_available_seats, num_of_total_seats) {
+  var red = 228, green = 142, blue = 0, ratio = num_of_available_seats / num_of_total_seats;
+  red += 16 * ratio;
+  red = convert_base10to16(red);
+  green += 18 * ratio;
+  green = convert_base10to16(green);
+  blue = (1 - ratio) * 140;
+  blue = convert_base10to16(blue);
+  return '#' + red + green + blue;
+}
+function get_colors() {
+  var building = 0,
+      is_same = false,
+      num_of_available_seats = 0,
+      num_of_total_seats = 0,
+      available_seats = "available_seats",
+      total_seats = "total_seats",
+      colors = new Array();
+  for (var id in ids) {
+    is_same = building == ids[id];
+    if (!is_same) {
+      colors[building] = get_color(num_of_available_seats, num_of_total_seats);
+      building = ids[id];
+      num_of_available_seats = 0;
+      num_of_total_seats = 0;
+    }
+    if (id == 'CD') {
+      num_of_available_seats = seats['C'][available_seats] + seats['D'][available_seats];
+      num_of_total_seats = seats['C'][total_seats] + seats['D'][total_seats];
+    } else {
+      num_of_available_seats += seats[id][available_seats];
+      num_of_total_seats += seats[id][total_seats];
+    }
+  }
+  colors[building] = get_color(num_of_available_seats, num_of_total_seats);
+  return colors;
+}
+function in_session_mode_contents(subject, lecturer) {
+  return new Array(subject, lecturer);
+}
+function open_mode_contents(prefix, num_of_available_seats, num_of_total_seats, available_still) {
+  return new Array(prefix + ":<tspan font-weight=\"bold\" fill=\"green\">" + num_of_available_seats + "</tspan>/" + num_of_total_seats, "-" + available_still);
+}
+function closed_mode_contents(pronoun, opening_hours_are, opening_time, closing_time, period) {
+  return new Array(pronoun + opening_hours_are, opening_time + "-" + closing_time + period);
 }
 function is_afternoon() {
   var is_afternoon = true, date = new Date();
   if (date.getHours() < 12) is_afternoon = false;
   return is_afternoon; 
 }
-function get_content(id, data, locale) {
-  var content = "",
+function get_contents(id, data, locale) {
+  var contents = new Array(),
       subject = "subject",
       lecturer = "lecturer",
       available_seats = "available_seats",
@@ -41,22 +109,18 @@ function get_content(id, data, locale) {
       today_closing_time = "today_closing_time";
   switch (data[id]["mode"]) {
   case 'in_session':
-    if (locale == 'en') {
-      content = in_session_mode_content(data[id][subject], data[id][lecturer], " is in session.");
-    } else {
-      content = in_session_mode_content(data[id][subject], data[id][lecturer], "が講義中です。");
-    }
+    contents = in_session_mode_contents(data[id][subject], data[id][lecturer]);
     break;
   case 'open':
-    if (locale == 'en') {
-      content = open_mode_content("Available Seats", seats[id][available_seats], seats[id][total_seats], data[id][available_still]);
+    if (locale == en) {
+      contents = open_mode_contents("Available Seats", seats[id][available_seats], seats[id][total_seats], data[id][available_still]);
     } else {
-      content = open_mode_content("空席", seats[id][available_seats], seats[id][total_seats], data[id][available_still]);
+      contents = open_mode_contents("空席", seats[id][available_seats], seats[id][total_seats], data[id][available_still]);
     }
     break;
   case 'closed':
     var pronoun = "", opening_time = "", closing_time = "", period = "";
-    if (locale == 'en') {
+    if (locale == en) {
       if (is_afternoon()) {
         pronoun = "Tomorrow";
         opening_time = data[id][tomorrow_opening_time];
@@ -68,9 +132,9 @@ function get_content(id, data, locale) {
       }
       period = ".";
       if ((opening_time == "") || (closing_time == "")) {
-        content = "Closed " + pronoun.toLowerCase() + period;
+        contents = new Array("Closed " + pronoun.toLowerCase() + period, "");
       } else {
-        content = closed_mode_content(pronoun, "'s opening hours are ", opening_time, closing_time, period);
+        contents = closed_mode_contents(pronoun, "'s opening hours are ", opening_time, closing_time, period);
       }
     } else {
       if (is_afternoon()) {
@@ -84,36 +148,53 @@ function get_content(id, data, locale) {
       }
       period = "です。";
       if ((opening_time == "") || (closing_time == "")) {
-        content = pronoun + "は閉室" + period;
+        contents = new Array( pronoun + "は閉室" + period, "");
       } else {
-        content = closed_mode_content(pronoun, "の開館時間は", opening_time, closing_time, period);
+        contents = closed_mode_contents(pronoun, "の開館時間は", opening_time, closing_time, period);
       }
     }
     break;
   default: break;
   }
-  return content;
+  return contents;
 }
 function set_data2map (data) {
   var room_name = 'room_name', more = '&gt;&gt;';
-  if (locale == 'en') {
+  if (locale == en) {
     room_name = 'english_room_name';
     more += "more";
   } else more += "詳細";
   for (var id in ids) {
-    var node = document.getElementById(id + '_room_name'), content = "";
+    var node = document.getElementById(id + '_room_name'), contents = new Array();
     if (id == 'CD') {
-      if (locale == 'en') {
+      if (locale == en) {
         node.innerSVG = "Machine Shop C &amp; D";
       } else {
         node.innerSVG = "ＰＣ教室ＣＤ";
       }
-      content = get_content('C', data, locale);
+      contents = get_contents('C', data, locale);
     } else {
-      node.innerSVG = data[id][room_name];
-      content = get_content(id, data, locale);
+      if (locale == en) {
+        if (id == 'N') {
+          node.innerSVG = "Seisan-tō Ⅰ 305";
+        } else if (id == 'J') {
+          node.innerSVG = "Center for Educational Research &amp;Practices";
+        } else if (id == 'K') {
+          node.innerSVG = "Denjō-tō 1F Terminal Room";
+        } else if (id == 'L') {
+          node.innerSVG = "Denjō-tō 3F Terminal Room 1";
+      	} else if (id == 'M') {
+          node.innerSVG = "Denjō-tō 3F Terminal Room 2";
+        } else {
+          node.innerSVG = data[id][room_name];
+        }
+	  } else {
+        node.innerSVG = data[id][room_name];
+      }
+      contents = get_contents(id, data, locale);
     }
-    document.getElementById(id + '_content').innerSVG = content;
+    document.getElementById(id + '_content_0').innerSVG = contents[0];
+    document.getElementById(id + '_content_1').innerSVG = contents[1]
     document.getElementById(id + '_more').innerSVG = more;
   }
 }
@@ -145,7 +226,6 @@ function get_rooms() {
   rooms[index] = prepend_room(str);
   return rooms;
 }
-var rooms = get_rooms();
 function get_node(room) {
   return document.getElementById(room);
 }
@@ -156,15 +236,15 @@ function focus_on(node, nodeb) {
   node.style.display = '';
   nodeb.setAttribute("fill", "red");
 }
-function blur(node, nodeb) {
+function blur(node, nodeb, room) {
   node.style.display = 'none';
-  nodeb.setAttribute("fill", "#F4A000");
+  nodeb.setAttribute("fill", colors[rooms.indexOf(room)]);
 }
 function hide_id(id) {
   hide(rooms[ids[id]]);
 }
 function hide(room) {
-  blur(get_node(room), get_nodeb(room));
+  blur(get_node(room), get_nodeb(room), room);
 }
 function hide_except(index) {
   for (var i = 0, room; room = rooms[i]; i++) {
@@ -178,7 +258,7 @@ function toggle(index) {
   if (node.style.display == 'none') {
     focus_on(node, nodeb);
   } else {
-    blur(node, nodeb);
+    blur(node, nodeb, room);
   }
 }
 function exe(index) {
