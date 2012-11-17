d63877fbf671127576492ce4bee5e279928c339a
ktr-skmt <sakamoto_911@hotmail.com>
2012-11-17 19:00:58 +0900

    update N and CD

--- roomO.js
+++
@@ -1,8 +0,0 @@
-var english_words = {"0":"Entrance"},
-    num_of_words = 0;
-for (var word in english_words) {num_of_words++;}
-if (locale == en) {
-  for (var i = 0; i < num_of_words; i++) {
-    document.getElementById('word_' + i).innerSVG = english_words[i];
-  }
-}
