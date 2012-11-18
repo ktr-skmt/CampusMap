f3e8746e55814c1e4444f479f16468f4413e9cdf
ktr-skmt <sakamoto_911@hotmail.com>
2012-11-18 10:58:24 +0900

    update roomCD.js

--- roomCD.js
+++ roomCD.js
@@ -1,11 +1,8 @@
-var base_url = 'http://pcavil.itsc.ynu.ac.jp/api.php?',
+var base_url = 'http://pcavil.itsc.ynu.ac.jp/api.php?room=',
     is_msie = $.browser.msie && window.XDomainRequest,
     locale = 'ja',
     en = 'en',
-    room_id = '',
-    url_c = base_url + 'room=C',
-    url_d = base_url + 'room=D';
-
+    room_ids = new Array('d', 'c');
 if (document.cookie) {
   var cookies = document.cookie.split("; ");
   for (var i = 0; i < cookies.length; i++) {
@@ -13,9 +10,6 @@ if (document.cookie) {
     if (str[0] == "locale") {
       var cookie_value = unescape(str[1]);
       if (cookie_value == en) locale = en;
-    } else if (str[0] == "room_id") {
-      var cookie_value = unescape(str[1]);
-      room_id = cookie_value;
     }
   }
 }
@@ -23,39 +17,30 @@ if (locale == en) {
   document.getElementById('japanese_words').style.display = 'none';
   document.getElementById('english_words').style.display = '';
 }
-if (is_msie) {
+function set_xdr(room_id) {
   var xdr = new XDomainRequest();
-  xdr.onload = function() {set_d_map(jQuery.parseJSON(xdr.responseText));}
-  xdr.open("get", url_d, true);
+  xdr.onload = function() {set_map(jQuery.parseJSON(xdr.responseText), room_id);}
+  xdr.open("get", url + room_id.toUpperCase(), true);
   xdr.send(null);
-  xdr = new XDomainRequest();
-  xdr.onload = function() {set_c_map(jQuery.parseJSON(xdr.responseText));}
-  xdr.open("get", url_c, true);
-  xdr.send(null);
+}
+if (is_msie) {
+  for (var i = 0; i < room_ids.length; i++) {set_xdr(room_ids[i]);}
 } else {
-  jQuery.getJSON(url_d, function(data) {set_d_map(data);});
-  jQuery.getJSON(url_c, function(data) {set_c_map(data);});
+  for (var i = 0; i < room_ids.length; i++) {
+    jQuery.getJSON(url + room_ids[i].toUpperCase(), function(data) {set_map(data, room_ids[i]);});
+  }
 }
 function turn_on(pc_id) {
   var id = 'pc_' + pc_id + '_';
   document.getElementById(id + 'on').style.display = '';
   document.getElementById(id + 'off').style.display = 'none';
 }
-function set_c_map(seats) {
+function set_map(seats, room_id) {
   var num_total_seats = seats['total_seats'],
       ids = seats['pc_id'];
   for (var pc_id = 1; pc_id <= num_total_seats; pc_id++) {
     for (var id in ids) {
-      if (ids[id] == pc_id) turn_on('c' + pc_id);
-    }
-  }
-}
-function set_d_map(seats) {
-  var num_total_seats = seats['total_seats'],
-    ids = seats['pc_id'];
-  for (var pc_id = 1; pc_id <= num_total_seats; pc_id++) {
-    for (var id in ids) {
-      if (ids[id] == pc_id) turn_on('d' + pc_id);
+      if (ids[id] == pc_id) turn_on(room_id + pc_id);
     }
   }
 }
